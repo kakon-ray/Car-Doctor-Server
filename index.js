01c0f35e2b3705dae6e6a153da1a958051b411e3
Kakon Ray <thisiskakonray@gmail.com>
2022-05-13 16:58:55 +0600

    cors error solve

--- index.js
+++ index.js
@@ -127,12 +127,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`CROUD server is Running ${port}`);
 });
-
-// CORS Error solve
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "application/json");
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
-});
